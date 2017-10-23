8b26761b3aee2aa9fb0d953b321b7fd1c82f4140
pcbt <batutr2@gmail.com>
2017-10-23 13:01:25 +0300

    outputs are remarkable now

--- auto_conf.py
+++ auto_conf.py
@@ -7,35 +7,42 @@ def python_bluez_installer():
     cmd_result=os.system(cmd);
     
     if cmd_result == 0:
-        print("Python Bluez Module is installed!")
+        print("########Python Bluez Module is installed!")
 
 def daemon_comp():
     cmd = sudo_mode + "cp dbus-org.bluez.service -t /etc/systemd/system"
     cmd_result=os.system(cmd);
     if cmd_result == 0:
-        print("Bluetooth Daemon is in compatibility mode!")
+        print("#####Bluetooth Daemon is in compatibility mode!")
+        cmd = 'systemctl daemon-reload'
+        cmd_result = os.system(cmd)
+        cmd = 'service bluetooth restart'
+        cmd_result = os.system(cmd)
         
 def load_serial_port():
     cmd = sudo_mode + "sdptool add SP"
     cmd_result=os.system(cmd);
     
     if cmd_result == 0:
-        print("Serial port profile loaded!")
+        print("######Serial port profile loaded!")
 
 def change_device_name():
     cmd = sudo_mode+"echo 'PRETTY_HOSTNAME=rpi' > /etc/machine-info"
     cmd_result=os.system(cmd);
     
     if cmd_result == 0:
-        print("Device name is changed! Now restarting Bluetooth!")
-        os.system("sudo service bluetooth restart")
+        print("#####Device name is changed! Now restarting Bluetooth!")
+        cmd = 'systemctl daemon-reload'
+        cmd_result = os.system(cmd)
+        cmd = 'service bluetooth restart'
+        cmd_result = os.system(cmd)
 
 def set_up_bl():
     cmd = sudo_mode+"bluetoothctl <<EOF\npower on\ndiscoverable on\npairable on\nagent NoInputNoOutput\ndefault-agent\nEOF"
     cmd_result=os.system(cmd);
     
     if cmd_result == 0:
-        print("Bluetooth is configured! Now restarting dhcpcd!")
+        print("######Bluetooth is configured! Now restarting dhcpcd!")
         cmd = 'systemctl daemon-reload'
         cmd_result = os.system(cmd)
         print cmd + " - " + str(cmd_result)
